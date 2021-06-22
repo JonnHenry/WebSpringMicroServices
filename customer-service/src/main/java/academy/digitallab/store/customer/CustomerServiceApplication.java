34b41a15a5c2ee40f0d95f6077bde3e47daca1dd
JonnHenry <jonnathanca9601@gmail.com>
2021-06-21 22:12:50 -0500

    Changes in the product-service

--- customer-service/src/main/java/academy/digitallab/store/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/academy/digitallab/store/customer/CustomerServiceApplication.java
@@ -2,10 +2,10 @@ package academy.digitallab.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
