f4f9d9b4d99cbd0a4c5d0f4a02dc0a1e119a18b3
JonnHenry <jonnathanca9601@gmail.com>
2021-06-21 22:47:19 -0500

    Connect microservices

--- shopping-service/src/main/java/academy/digitallab/store/shopping/model/Customer.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/model/Customer.java
@@ -17,22 +17,11 @@ import javax.validation.constraints.Size;
 @Builder
 public class Customer {
     private Long id;
-
-
     private String numberID;
-
     private String firstName;
-
-
     private String lastName;
-
-
     private String email;
-
-
     private String photoUrl;
-
     private Region region;
-
     private String state;
 }
--- shopping-service/src/main/java/academy/digitallab/store/shopping/model/Product.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/model/Product.java
@@ -12,13 +12,10 @@ import java.util.Date;
 @Data
 public class Product {
     private Long id;
-
     private String name;
     private String description;
-
     private Double stock;
     private Double price;
     private String status;
-
     private Category category;
 }
